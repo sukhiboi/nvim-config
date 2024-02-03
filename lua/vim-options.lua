a8a06567fadb56b52788f236eea2c3f0c6c7e217
sukhiboi <41sukhdevsingh@gmail.com>
2024-02-03 21:09:19 +0530

    Add some extra plugin for better editor experince

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,4 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set cursorline")
+vim.cmd("set nu")
+vim.cmd("set colorcolumn=80")
 vim.g.mapleader = " "
